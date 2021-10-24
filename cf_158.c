ad7cb8fe701c36e9885f3c62aad06044941697a1
AlexDras <72723818+AlexDras@users.noreply.github.com>
2021-10-24 12:53:12 +0300

    test upload

---
+++ cf_158.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define max_sum 1000000007
+
+int main(void)
+{
+    unsigned long long count_inp, number, out_summ;
+    double base;
+    for (scanf("%llu", &count_inp); count_inp > 0; count_inp--) {
+        out_summ = 0;
+        scanf("%lf %llu", &base, &number);
+        for (unsigned long long i = 0; i < 32; i++) {
+            if (number & (1 << i)) {
+                out_summ += pow(base, i);
+                while (out_summ >= max_sum) {
+                    out_summ -= max_sum;
+                }
+            }
+        }
+        printf("%llu\n", out_summ);
+
+    }
+    return 0;
+}
