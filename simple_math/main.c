e989e41c365010c54b8657b1d3f3044bcca02ac7
Maxim252 <93118217+Maxim252@users.noreply.github.com>
2021-11-07 21:07:14 +0300

    добавил скелет
    написал кучу

--- simple_math/main.c
+++ simple_math/main.c
@@ -9,9 +9,80 @@
 //    Аноним Ананисус
 //--------------------------------------------------------------
 
-#include <"headers.h">
+#include "headers.h"
 
-int main(int argc, char const *argv[])
+//long int matrix_1[3][3];
+//int width_1, height_1;
+
+
+
+void matrix_addition() {//.Сложение матриц
+
 }
-  return 0;
+
+void matrix_subtraction() {//Вычитание матриц
+
 }
+
+void matrix_multiplication() {//Умножение матриц
+
+}
+
+void transposing_matrices() {//Транспонирование матриц
+
+}
+
+void inverse_matrix() {//Обратная матрица
+
+}
+
+void acs_equations() {//Уравнения СЛАУ
+
+}
+
+int main(){
+    int otvet=0,repeat=0; // переменная для сохранение ответа
+    char nomer; //В данной переменной сохрается номер выбранной пользователем число, и потом прогоняется по switch...
+    setlocale(LC_ALL, "Russian");
+    printf("%s%s", main_prefix, "Вас приветствует калькулятор! \nВыберите действие, которое вы хотите провести с матрицей. (Напишите цифру без точки)\n");
+    puts("1.Сложение матриц");
+    puts("2.Вычитание матриц");
+    puts("3.Умножение матриц");
+    puts("4.Транспонирование матриц");
+    puts("5.Обратная матрица");
+    puts("6.Уравнения СЛАУ");
+    scanf("%c", &nomer);
+        switch (nomer) {
+        case '1':
+            matrix_addition();
+            printf("%s", "1");
+            break;
+        case '2':
+            matrix_subtraction();
+            printf("%s", "");
+            break;
+        case '3':
+            matrix_multiplication();
+            printf("%s", "");
+            break;
+        case '4':
+            transposing_matrices();
+            printf("%s", "");
+            break;
+        case '5':
+            inverse_matrix();
+            printf("%s", "");
+            break;
+        case '6':
+            printf("%s", "");
+            break;
+        case '7':
+            acs_equations();
+            break;
+        default:
+            printf("%s", "Ошибка.Введите заново цифру.");
+            break;
+        }
+       
+    }
+
